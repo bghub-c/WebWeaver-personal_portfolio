74dbe621bd6f98481c4d5cbcbbf22c61c259bcb7
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-05 03:14:52 +0530

    Ek animation ne 3baja diye

--- src/App.jsx
+++ src/App.jsx
@@ -8,109 +8,7 @@ function App() {
       id="mainwrapper"
       className="w-screen text-white h-full flex flex-col items-center justify-center selection:bg-black selection:text-muted scroll-smooth"
     >
-      <div className="absolute w-screen h-screen top-0 text-accentylt z-1">
-        <svg
-        className="bg-white text-zinc-900 hover:bg-bkgggg hover:text-white transition-all duration-700 "
-          id="visual"
-          viewBox="0 0 1440 824"
-          width="100vw"
-          height="100vh"
-          xmlns="http://www.w3.org/2000/svg"
-          version="1.1"
-        >
-          <g className="opacity-50">
-            <g transform="translate(800, 40) rotate(65) scale(0.5)">
-              <path
-                d="M0 -129.5L112.2 64.8L-112.2 64.8Z"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="0.5%"
-              ></path>
-            </g>
-            <g transform="translate(650 45) rotate(25)">
-              <path
-                d="M0 -129.5L112.2 64.8L-112.2 64.8Z"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="0.5%"
-              ></path>
-            </g>
-            <g transform="translate(267 490)">
-              <rect
-                x="-50"
-                y="-50"
-                width="100"
-                height="100"
-                stroke="currentColor"
-                strokeWidth="0.5%" 
-                fill="none"
-              ></rect>
-            </g>
-            <g transform="translate(750 600)">
-              <circle
-                cx="0"
-                cy="0"
-                r="150"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="0.5%"
-              ></circle>
-            </g>
-            <g transform="translate(380 120) rotate(20)">
-              <rect
-                x="-30"
-                y="-30"
-                width="55"
-                height="55"
-                stroke="currentColor"
-                strokeWidth="0.5%" 
-                fill="none"
-              ></rect>
-            </g>
-            <g transform="translate(45 120) ">
-              <circle
-                cx="0"
-                cy="0"
-                r="100"
-                stroke="currentColor"
-                strokeWidth="0.5%"                fill="none"
-              ></circle>
-            </g>
-            <g transform="translate(595 300) rotate(78)">
-              <rect
-                x="-20"
-                y="-20"
-                width="30"
-                height="30"
-                stroke="currentColor"
-                strokeWidth="0.5%" 
-                fill="none"
-              ></rect>
-            </g>
-            <g transform="translate(750 200)">
-              <circle
-                cx="0"
-                cy="0"
-                r="30"
-                stroke="currentColor"
-                strokeWidth="0.5%" 
-                fill="none"
-              ></circle>
-            </g>
-            <g transform="translate(150 450)">
-              <rect
-                x="-25"
-                y="-25"
-                width="50"
-                height="50"
-                stroke="currentColor"
-                strokeWidth="0.5%" 
-                fill="none"
-              ></rect>
-            </g>
-          </g>
-        </svg>
-      </div>
+      
 
       {/* 
      import { Code } from "@phosphor-icons/react"
@@ -135,7 +33,7 @@ import { motion } from 'framer-motion'
       weight="light"
       />
       </motion.div>  */}
-      <div className="w-screen h-screen flex justify-center items-center">
+      <div className="app w-screen h-screen flex justify-start items-end">
         <Home />
       </div>
       <div className="w-screen h-screen ">
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,57 +2,73 @@
 
 import { motion } from "framer-motion";
 export function Home() {
-  const spaneleh = (s,classes) => {
-    return <span className={classes}>{s}</span>;
-  };
-  const spanelef = (s,classes) => {
-    return <span className={classes}>{s}</span>;
+  const icon = {
+    hidden: {
+      pathLength: 0,
+      fill: "rgba(255, 255, 255, 0)",
+    },
+    visible: {
+      pathLength: 1,
+      fill: "rgba(255, 255, 255, 1)",
+    },
   };
   return (
     <>
-      <section
-        className="hidden w-3/4 h-5/6	 z-2 backdrop-blur-md border-l border-accentylt"
-      >
-        
-        <motion.span 
-        initial={{
-          opacity:0,
-          x:-10
-        }}
-        animate={{
-          opacity:1,
-          x:10
-        }}
-        transition={{
-          delay:1.95
-          }}
-        className="w-full h-1/2 pl flex flex-col justify-center">
-          {spaneleh("<h2 id='Hello_world'>","text-3xl laptop:text-xl text-white vt323h")}
-          <p className="pl text-3xl smartphone:text-lg text-accentylt vt323">{"Hii!!"}</p>
-          {spanelef("</ h2>","text-3xl laptop:text-xl text-white vt323h")}
-        </motion.span>
+      <section className="w-full grid-flow-row grid-rows-auto ml-1	z-2 border-accentylt overflow-hidden">
         <motion.span
-        initial={{
-          opacity:0,
-          x:-10
-        }}
-        animate={{
-          opacity:1,
-          x:10
-        }}
-        transition={{
-          delay:2.10
-          }} 
-        className="z-10 row-start-2 flex flex-col justify-start">
-        {spaneleh("<h1 id='Yupp!_It's_me'>","text-3xl laptop:text-xl  text-white mt-10 ml-10 vt323h")}
-          <p className="pl  text-7xl laptop:text-3xl text-accentylt vt323">
-            I&#x27;m Mohd Bilal
-            <br />A passionate web developer 
+          initial={{
+            y: -100,
+          }}
+          animate={{
+            y: 0,
+          }}
+          transition={{
+            delay: 1.95,
+          }}
+          className="w-full h-1/2 pl flex flex-col justify-center"
+        >
+          <p className="pl text-7xl smartphone:text-lg text-accentylt vt323">
+            {"Nice To See You!"}
           </p>
-          {spanelef("</ h1>", "text-3xl laptop:text-xl  text-white mb-10 ml-10 vt323h")}
         </motion.span>
-        <span className="z-10 flex flex-col opacity-100">
-          
+
+        <motion.span
+          initial={{
+            y: 500,
+          }}
+          animate={{
+            y: 0,
+          }}
+          transition={{
+            delay: 2.9,
+          }}
+          className="pl p-7 z-10 row-start-2 flex flex-col justify-start"
+        >
+          <p className="pl  text-7xl laptop:text-3xl text-accentylt vt323">
+            👋 Hi! My name is Mohd Bilal <br /> and I&#x27;m a Frontend web
+            developer
+          </p>
+        </motion.span>
+        <span className="grid place-content-center">
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100"
+            viewBox="0 0 700 824"
+            className="item"
+          >
+            {/* m 290 0 v 200 h 160 l -180 150 l -170 -150 h 160 v -200 h 30  */}
+            <motion.path
+              d="m 290 0 v 200 h 160 l -180 150 l -170 -150 h 160 v -200 h 30"
+              variants={icon}
+              initial="hidden"
+              animate="visible"
+              whileHover="visible"
+              transition={{
+                default: {delay:4, duration: 2.5, ease: "easeInOut" },
+                fill: {delay:4.4, duration: 3.5, ease: [1, 0, 0.8, 1] },
+              }}
+            />
+          </motion.svg>
         </span>
       </section>
     </>
