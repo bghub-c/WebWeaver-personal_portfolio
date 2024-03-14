f0b69db36b9aa213a6d1e65671fd1eef1f43eaea
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-15 02:19:57 +0530

    App changes

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -16,7 +16,7 @@ export function Navbar(){
             delay:0.5,
             ease:"easeInOut"
         }}
-        className="w-screen h-nav backdrop-blur-xl text-white flex tablet:flex-col justify-between items-center sticky top-0">
+        className="w-screen h-nav backdrop-blur-xl pb-1 text-white flex tablet:flex-col justify-between items-center sticky top-0">
             <h1 className="vt323h  text-5xl">{"</ "}MohdBilal{">"}</h1>
             <ul className="flex backdrop-blur-lg">
                 <li className="h-full w-1/5 m-3 backdrop-brightness-200 text-white transition-all ease-in-out duration-200 hover:scale-105 hover:-translate-y-1 hover:text-insta p-3/4"><InstagramLogo size={32} weight="duotone" /></li>
