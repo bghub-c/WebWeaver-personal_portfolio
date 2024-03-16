dc0ce0fe504316c7dc6657871a8a9c88231ba5d7
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-16 23:56:41 +0530

    zfzd

--- src/Pages/Introduction.jsx
+++ src/Pages/Introduction.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import pfp from '../assets/billu.jpg'
+import pfp from "../assets/billu.jpg";
 export function Introduction() {
   return (
     <>
@@ -10,12 +10,47 @@ export function Introduction() {
         className="h-full w-screen flex justify-center bg-black"
       >
         <div className="w-1/2">
-            <h1  className="text-8xl coolfont ml-6 mt-60 text-whitee flex items-end">Hello There <span className="text-redd ml-4">!</span></h1>
-            <motion.h2 initial={{y:300}} animate={{y:0}} transition={{delay:.80, duration:0.3, type:"spring" }} className=" mt-16 text-4xl text-whitee ubuntu">I am <span className="coolfont mr-2 text-redd">Mohd Bilal</span>, a B.Tech student deeply passionate about<br /><span className=" tracking-widestestcum ">web development</span>, I embody a genuine <span className=" tracking-widestestcum ">enthusiasm</span>  for crafting exceptional digital experiences.</motion.h2>
-            <motion.div initial={{y:320}} animate={{y:0}} transition={{delay:1.2, duration:0.4, type:"spring" }} className="w-full mt-12 grid grid-flow-col smartphone:grid-flow-row"> 
-            <p className="text-whitee  text-2xl tracking-tighter mt-6 ibm-plexregular leading-10">{" You can call me an"} <span className="text-redd">{"enthusiast workaholic"}</span>{", or maybe just a web wizard in the making. With a relentless passion for coding and creating. I'm on mission to keep ethical standards in check while sprinkling a dash of creativity into every project, all about delivering results that make your jaw drop and your users say "}<span className="tracking-wide text-redd font-semibold">{"'Wow'"}</span>{".So, if you want a web developer who's a little too excited about crafting digital magic, you've found your guy."}</p>
-            <div> <img className="h-3/4 translate-x-16 -translate-y-5 rounded-lg grayscale hover:grayscale-0 transition-all duration-700 overflow-hidden" src={pfp} alt="" /></div>
-            </motion.div>
+          <h1 className="text-7xl coolfont ml-6 mt-60 text-whitee flex items-end">
+            Hello There <span className="text-redd ml-4">!</span>
+          </h1>
+          <motion.h2
+            initial={{ y: 300 }}
+            animate={{ y: 0 }}
+            transition={{ delay: 0.8, duration: 0.3, type: "spring" }}
+            className=" mt-16 text-3xl text-whitee ubuntu"
+          >
+            Ahoy! <span className="coolfont mr-2 ml-2 text-redd">Mohd Bilal</span>, your web wizard from Lucknow, India. Currently enrolled in B.Tech .
+            <span className=" tracking-widestestcum ml-1">Crafting</span>,{" digital marvels? You bet! Join me, and"} <br /> {"let's "}
+            <span className=" tracking-widestestcum ml-1 ">weave wonders</span> together. Cheers!
+          </motion.h2>
+          <motion.div
+            initial={{ y: 320 }}
+            animate={{ y: 0 }}
+            transition={{ delay: 1.2, duration: 0.4, type: "spring" }}
+            className="w-full mt-12 grid grid-flow-col smartphone:grid-flow-row"
+          >
+            <p className="text-whitee  text-2xl tracking-tighter mt-6 ibm-plexregular leading-10">
+              {" You can call me an"}{" "}
+              <span className="text-redd">{"enthusiast workaholic"}</span>
+              {
+                ", or maybe just a web wizard in the making. With a relentless passion for coding and creating. I'm on mission to keep ethical standards in check while sprinkling a dash of creativity into every project, all about delivering results that make your jaw drop and your users say "
+              }
+              <span className="tracking-wide text-redd font-semibold">
+                {"'Wow'"}
+              </span>
+              {
+                ".So, if you want a web developer who's a little too excited about crafting digital magic, you've found your guy."
+              }
+            </p>
+            <div>
+              {" "}
+              <img
+                className="h-3/4 translate-x-16 -translate-y-5 rounded-lg grayscale hover:grayscale-0 transition-all duration-700 overflow-hidden"
+                src={pfp}
+                alt=""
+              />
+            </div>
+          </motion.div>
         </div>
       </motion.section>
     </>
