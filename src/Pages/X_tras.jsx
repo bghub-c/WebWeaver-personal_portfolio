62b51d69bd18c4982beff0cf5c6ff69a70bc8fd1
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-19 01:30:30 +0530

    haha changes

--- src/Pages/X_tras.jsx
+++ src/Pages/X_tras.jsx
@@ -1,33 +1,90 @@
 export function X_tras(){
     const cards = [
         {
-          url: "./assests/IMG_20210105_092430 (1).jpg",
+          url: 'src/Pages/assets/IMG_20210105_092430 (1).jpg',
           title: "Title 1",
+          subtitle: "kuch bhi",
+          css: "w-96 p-4",
+          divcss:"w-1/2",
           id: 1,
         },
         {
-          url: "./assests/IMG_20210314_182747 (5).jpg",
-          title: "Title 2",
-          id: 2,
-        },
-        {
-          url: "../assets/IMG_20210314_182747 (5).jpg",
-          title: "Title 3",
-          id: 3,
-        },
+            url: 'src/Pages/assets/IMG_2386.jpg',
+            title: "Title 2",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 2,
+          },
+          {
+            url: 'src/Pages/assets/IMG_20210314_182747 (5).jpg',
+            title: "Title 13",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 3,
+          },
+          {
+            url: 'src/Pages/assets/IMG_20221104_181302_078.jpg',
+            title: "Title 14",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 4,
+          },
+          {
+            url: 'src/Pages/assets/IMG_20240313_031952_169.jpg',
+            title: "Title 15",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 5,
+          },
+          {
+            url: 'src/Pages/assets/PXL_20220119_042449211.jpg',
+            title: "Title 16",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 6,
+          },
+          {
+            url: 'src/Pages/assets/PXL_20230625_173016981~2.jpg',
+            title: "Title 17",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 7,
+          },
+          {
+            url: 'src/Pages/assets/PXL_20220715_090429594~2.jpg',
+            title: "Title 18",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 8,
+          },
+          {
+            url: 'src/Pages/assets/Remini20211224162741784.jpg',
+            title: "Title 19",
+            subtitle: "kuch bhi",
+            css: "w-96 overflow-hidden p-4",
+            divcss:"w-1/2",
+            id: 9,
+          },
       ];
     return(<section id="xtramain" className="flex flex-col items-center bg-whitee text-black w-screen h-full">
-        <section className="w-11/12 bg-red-600 flex flex-col items-center justify-center">
+        <section id="photography" className="w-11/12 bg-red-600  items-center justify-center">
         <div id="text">
             <h1>Photography</h1>
             <h2>Scroll Down to see the work</h2>
         </div>
-        <div id="pics" className=" flex flex-wrap gap-1 justify-center items-center">
+        <div id="pics" className="flex flex-wrap gap-5 justify-center items-center">
            {cards.map((card)=>{
                 return(
-                    <div key={card.id} className="w-full bg-white">
-                            <img key={card.id} src={card.url} alt={`image-${card.id}`} className="w-full h-full" />
-                            <h1>{card.title}</h1>
+                    <div key={card.id} className="flex w-fit h-96 flex-col bg-white">
+                            <div style={{backgroundImage:`url(${card.url})`}} className="w-96 h-96 m-4 mb-10 bg-cover bg-no-repeat"></div>
+                            <h1 className="">{card.title}</h1>
                     </div>
 
                 )
