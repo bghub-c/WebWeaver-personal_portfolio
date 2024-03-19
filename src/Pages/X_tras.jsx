823fcce8c461268331946a18bdd1f25b36ca5372
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-20 02:35:57 +0530

    Added Pic corousel

--- src/Pages/X_tras.jsx
+++ src/Pages/X_tras.jsx
@@ -5,62 +5,122 @@ export function X_tras() {
   const { scrollYProgress } = useScroll({
     target: targetRef,
   });
-  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
+  
+
+  const x = useTransform(scrollYProgress, [0.20, 1], ["1%", "-95%"]);
   const cards = [
     {
-      url: "src/Pages/assets/pic1.jpg",
+      url: "src/Pages/assets/pic1.jpeg",
       title: "Title 1",
       subtitle: "kuch bhi",
       id: 1,
     },
     {
-      url: "src/Pages/assets/pic2.jpg",
+      url: "src/Pages/assets/pic21.jpg",
       title: "Title 12",
       subtitle: "kuch bhi",
       id: 2,
     },
     {
-      url: "src/Pages/assets/pic3.jpg",
+      url: "src/Pages/assets/picc4.jpg",
       title: "Title 13",
       subtitle: "kuch bhi",
       id: 3,
     },
 
     {
-      url: "src/Pages/assets/pic4.jpg",
+      url: "src/Pages/assets/picc5.jpg",
       title: "Title 14",
       subtitle: "kuch bhi",
       id: 4,
     },
     {
-      url: "src/Pages/assets/pic5.jpg",
+      url: "src/Pages/assets/picc6.jpg",
       title: "Title 15",
       subtitle: "kuch bhi",
       id: 5,
     },
     {
-      url: "src/Pages/assets/pic6.jpg",
+      url: "src/Pages/assets/picc7.jpg",
       title: "Title 16",
       subtitle: "kuch bhi",
       id: 6,
     },
     {
-      url: "src/Pages/assets/pic7.jpg",
+      url: "src/Pages/assets/picc8.jpg",
       title: "Title 17",
       subtitle: "kuch bhi",
       id: 7,
     },
     {
-      url: "src/Pages/assets/pic8.jpg",
+      url: "src/Pages/assets/picc9.jpg",
       title: "Title 18",
       subtitle: "kuch bhi",
       id: 8,
     },
     {
-      url: "src/Pages/assets/pic9.jpg",
+      url: "src/Pages/assets/picc10.jpg",
       title: "Title 19",
       subtitle: "kuch bhi",
       id: 9,
+    },
+    {
+      url: "src/Pages/assets/pic11.jpg",
+      title: "Title 20",
+      subtitle: "kuch bhi",
+      id: 10,
+    },
+    {
+      url: "src/Pages/assets/pic12.jpg",
+      title: "Title 21",
+      subtitle: "kuch bhi",
+      id: 11,
+    },
+    {
+      url: "src/Pages/assets/pic13.jpg",
+      title: "Title 22",
+      subtitle: "kuch bhi",
+      id: 12,
+    },{
+      url: "src/Pages/assets/pic14.jpg",
+      title: "Title 23",
+      subtitle: "kuch bhi",
+      id: 13,
+    },{
+      url: "src/Pages/assets/pic15.jpg",
+      title: "Title 24",
+      subtitle: "kuch bhi",
+      id: 14,
+    },{
+      url: "src/Pages/assets/pic16.jpg",
+      title: "Title 25",
+      subtitle: "kuch bhi",
+      id: 15,
+    },{
+      url: "src/Pages/assets/pic21.jpg",
+      title: "Title 26",
+      subtitle: "kuch bhi",
+      id: 16,
+    },{
+      url: "src/Pages/assets/pic18.jpg",
+      title: "Title 27",
+      subtitle: "kuch bhi",
+      id: 17,
+    },{
+      url: "src/Pages/assets/pic20.jpg",
+      title: "Title 28",
+      subtitle: "kuch bhi",
+      id: 18,
+    },{
+      url: "src/Pages/assets/pic23.jpg",
+      title: "Title 29",
+      subtitle: "kuch bhi",
+      id: 19,
+    },{
+      url: "src/Pages/assets/pic24.jpg",
+      title: "Title 30",
+      subtitle: "kuch bhi",
+      id: 20,
     },
   ];
   return (
@@ -74,26 +134,23 @@ export function X_tras() {
       >
         <section ref={targetRef} className="relative h-[300vh] smartphone:h-[500vh]">
         <div id="text">
-          <h1 className="text-8xl smartphone:text-4xl m-16 smartphone:m-10  mt-32 smartphone:mt-10  coolfont">Photography</h1>
-          <h2 className="text-3xl smartphone:text-lg ml-24 smartphone:ml-16  ubuntu">:) Some clicks that {"I'm"} proud of</h2>
+          <h1 className="text-8xl smartphone:text-5xl m-16 smartphone:m-10  mt-32 smartphone:mt-10  coolfont">Photography</h1>
+          <h2 className="text-3xl smartphone:text-xl ml-24 smartphone:ml-16  ubuntu">:) Some clicks that {"I'm"} proud of</h2>
         </div>
-          <div className="sticky top-0 flex h-1/5 items-center overflow-hidden">
-            <motion.div style={{ x }} className="flex gap-4">
+          <div className="sticky top-0 mt-8 flex h-1/5 items-center overflow-hidden">
+            <motion.div style={{ x }} className="flex  gap-4  transition-transform ease-in-out duration-1000">
               {cards.map((card) => {
                 return (
                   <div
                     key={card.id}
-                    className="group rounded-sm relative h-[500px] w-[500px] smartphone:h-[500px] smartphone:w-[350px] overflow-hidden bg-neutral-200"
+                    className="group rounded-sm relative h-[500px] w-[350px] smartphone:h-[500px] smartphone:w-[250px] overflow-hidden bg-neutral-200"
                   >
-                    <div
-                      style={{
-                        backgroundImage: `url(${card.url})`,
-                        backgroundSize: "cover",
-                        backgroundPosition: "center",
-                      }}
-                      className="absolute inset-0 z-0 transition-transform duration-100 ease-in-out group-hover:scale-110"
-                    ></div>
-                    <div className={` absolute bottom-2 left-2 z-10 place-content-center translate-y-24 group-hover:-translate-y-2 grid transition-transform duration-300 ease-in-out`}>
+                    <img
+                     src={card.url}
+                     loading="lazy"
+                      className="absolute w-full h-full object-cover inset-0 z-0 transition-all duration-500 ease-in-out group-hover:scale-110"
+                    ></img>
+                    <div className={` absolute bottom-2 left-2 z-10 place-content-center translate-y-24 group-hover:-translate-y-2 grid transition-transform duration-700 ease-in-out`}>
                       <p className="bg-gradient-to-bl from-black/20 to-white/0 p-4 text-2xl font-black uppercase text-white backdrop-blur-xl">
                         {card.title}
                       </p>
