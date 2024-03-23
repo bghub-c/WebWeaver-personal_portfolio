73ba408cab37120bb963acb3952db12da86f7f50
Mohd Bilal <bilalmail7054@gmail.com>
2024-03-24 03:04:17 +0530

    Work updates

--- src/Pages/Work.jsx
+++ src/Pages/Work.jsx
@@ -2,8 +2,8 @@ import { Barricade } from "@phosphor-icons/react"
 export function Work(){
     return( <>
         <section className="w-screen h-screen flex justify-center items-center text-whitee">
-        <Barricade className="mx-4 text-redd" size={100} weight="duotone" /> <br />
-        <h1 className="text-7xl  ubuntu">Coming Soon <span className="text-redd">!</span></h1>
+        <Barricade className="mx-4 text-red-600" size={"15%"} weight="duotone" /> <br />
+        <h1 className="text-7xl smartphone:text-5xl  ubuntu">Coming Soon <span className="text-red-600">!</span></h1>
         </section>
         </>
     )
